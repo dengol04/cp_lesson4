549c8c39fc46f721e11c0949ba2af436c287f9d6
Denis Golubov <dengol04@mail.ru>
2026-09-15 16:44:26 +0300

    The differentiation function is implemented

--- src/miniMaple.js
+++ src/miniMaple.js
@@ -1,5 +1,88 @@
 class MiniMaple{
+    static diff(polynomial, variable) {
+        const monomials = polynomial.match(/(?:[\+-]?\d*)?\*?(?:[^\d\^\*\+-]+)(?:\^-?\d+)?/g)
+        let diffed_polynomial = ""
+        for (const monomial of monomials) {
+            diffed_polynomial += this.#diff_monomial(monomial, variable)
+        }
 
+        if (diffed_polynomial && diffed_polynomial[0] == "+")
+            diffed_polynomial = diffed_polynomial.slice(1)
+
+        return diffed_polynomial === "" ? "0" : diffed_polynomial
+    }
+
+    static monomial_parts = monomial => monomial.match(/^([\+-]?\d*)?\*?([^\d\^\*\+-]+)(\^-?\d+)?$/)
+
+    static #diff_monomial(monomial, variable) {
+        let coef = this.#find_coef(monomial)
+        let diff_var = this.#find_diff_var(monomial)
+        let pow = this.#find_pow(monomial)
+
+        if (variable !== diff_var)
+            return ""
+
+        let diffed_coef = coef * pow
+        let diffed_pow = pow - 1
+
+        let diffed_monomial = this.#assemble_monomial(diffed_coef, variable, diffed_pow)
+
+        return diffed_monomial
+    }
+
+    static #assemble_monomial(coef, variable, pow) {
+        let monomial = ""
+
+        if (coef === 0)
+            return ""
+
+        if (pow === 0)
+            return coef.toString()
+
+        if (coef !== 1)
+            if (coef === -1)
+                monomial += "-"
+            else {
+                if (coef > 0)
+                    monomial += "+"
+                monomial += coef + "*"
+            }
+        
+        monomial += variable
+
+        if (pow !== 1)
+            monomial += "^" + pow
+        
+        return monomial
+    }
+
+    static #find_coef(monomial) {
+        let coef = this.monomial_parts(monomial)[1]
+
+        if (coef === "" || coef === undefined)
+            return 1
+        else {     
+            if (coef === "-")
+                return -1
+
+            return Number(coef)
+        }
+    }
+
+    static #find_pow(monomial) {
+        let pow = this.monomial_parts(monomial)[3]
+        
+        if (pow === "" || pow == undefined)
+            return 1
+        else {
+            pow = pow.slice(1)
+            return Number(pow)
+        }
+    }
+
+    static #find_diff_var(monomial) {
+        return this.monomial_parts(monomial)[2]
+    }
 }
 
 export {MiniMaple}
--- test/miniMaple.test.js
+++ test/miniMaple.test.js
@@ -1,5 +1,76 @@
-import {MiniMaple} from "../src/miniMaple";
+import { MiniMaple } from "../src/miniMaple.js";
 
-test('it fails', () => {
-    expect(false).toBeTruthy();
+test('4*x^3, x', () => {
+    let res = MiniMaple.diff("4*x^3", "x");
+    expect(res).toBe("12*x^2");
+});
+
+test('4*x^3, y', () => {
+    let res = MiniMaple.diff("4*x^3", "y");
+    expect(res).toBe("0");
+});
+
+test('4*x^3-x^2, x', () => {
+    let res = MiniMaple.diff("4*x^3-x^2", "x");
+    expect(res).toBe("12*x^2-2*x");
+});
+
+test('5*x, x', () => {
+    let res = MiniMaple.diff("5*x", "x");
+    expect(res).toBe("5");
+});
+
+test('x^2, x', () => {
+    let res = MiniMaple.diff("x^2", "x");
+    expect(res).toBe("2*x");
+});
+
+test('-3*x^4, x', () => {
+    let res = MiniMaple.diff("-3*x^4", "x");
+    expect(res).toBe("-12*x^3");
+});
+
+test('2*x^3+5*x^2-7*x, x', () => {
+    let res = MiniMaple.diff("2*x^3+5*x^2-7*x", "x");
+    expect(res).toBe("6*x^2+10*x-7");
+});
+
+test('2*x^3+3*y^2, x', () => {
+    let res = MiniMaple.diff("2*x^3+3*y^2", "x");
+    expect(res).toBe("6*x^2");
+});
+
+test('2*x^3+3*y^2, y', () => {
+    let res = MiniMaple.diff("2*x^3+3*y^2", "y");
+    expect(res).toBe("6*y");
+});
+
+test('5*y^3, x', () => {
+    let res = MiniMaple.diff("5*y^3", "x");
+    expect(res).toBe("0");
+});
+
+test('3*alpha^2, alpha', () => {
+    let res = MiniMaple.diff("3*alpha^2", "alpha");
+    expect(res).toBe("6*alpha");
+});
+
+test('x^-2, x', () => {
+    let res = MiniMaple.diff("x^-2", "x");
+    expect(res).toBe("-2*x^-3");
+});
+
+test('4*x^3+5*y^2, x', () => {
+    let res = MiniMaple.diff("4*x^3+5*y^2", "x");
+    expect(res).toBe("12*x^2");
+});
+
+test('5*y^3+2*y, x', () => {
+    let res = MiniMaple.diff("5*y^3+2*y", "x");
+    expect(res).toBe("0");
+});
+
+test('-4*x^3-2*x^2, x', () => {
+    let res = MiniMaple.diff("-4*x^3-2*x^2", "x");
+    expect(res).toBe("-12*x^2-4*x");
 });
